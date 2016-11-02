32ded9975d1f0dde985ffae428d2166667ea16ed
Ahmed Ashraf <ahmed29329@gmail.com>
2016-11-02 05:29:35 +0200

    Create 721.cpp

---
+++ A/721.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<algorithm>
+#include <cstring>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    char word[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> word[i];
+    }
+    
+    
+    vector<int> m;
+    
+    int cm = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(word[i] == 'B' && word[i - 1] == 'B'){
+            m[cm] += 1;
+            if(word[i + 1] == 'W')
+            {
+                cm++;
+            }
+        } else if(word[i] == 'B'){
+            m.push_back(1);
+            if(word[i + 1] == 'W')
+            {
+                cm++;
+            }
+        }
+        
+    }
+    
+    cout << m.size();
+    
+    if(m.size() != 0)
+        cout << "\n";
+        
+    for(int i = 0; i < m.size(); i++)
+    {
+        cout << m[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
